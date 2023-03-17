807c04e1b5ea70ac09283dafb29412f0080d3da5
Fizruk <fizrukprogram@gmail.com>
2023-03-17 22:39:05 +0100

    в

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,18 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+const galleryWar = document.querySelector(`.gallery`);
+
+const qalleryItemMarkup = galleryItems.map(({praview, orginal, description}) =>
+<div class="gallery__item">
+  <a class="gallery__link" href="${original}"> 
+  <img 
+  class="gallery__image"
+  src="${preview}"
+  data-source="${original}"
+  alt="${description}"
+/>
+</a> 
+</div>).join('');
+
+galleryWrap.innerHTML=galleryItemMarkup;
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,14 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryWar = document.querySelector(`.gallery`);
+
+const qalleryItemMarkup = galleryItems.map(({praview, orginal, description}) =>
+<li class="gallery__item">
+   <a class="gallery__link" href="large-image.jpg">
+      <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+   </a>
+</li>).join('');
+
+galleryWrap.innerHTML=galleryItemMarkup;
 console.log(galleryItems);
