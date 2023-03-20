64dab75b8b1e90dae3b092030fa8a7d10732f0e6
Fizruk <fizrukprogram@gmail.com>
2023-03-20 14:07:33 +0100

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,6 @@
-import { galleryItems } from './gallery-items.js';
+import  galleryItems  from './gallery-items.js';
 const galleryWar = document.querySelector(`.gallery`);
+
 
 const qalleryItemMarkup = galleryItems.map(({praview, orginal, description}) =>
 <div class="gallery__item">
@@ -13,6 +14,20 @@ const qalleryItemMarkup = galleryItems.map(({praview, orginal, description}) =>
 </a> 
 </div>).join('');
 
+
 galleryWrap.innerHTML=galleryItemMarkup;
 
 console.log(galleryItems);
+
+import * as basicLightbox from 'basiclightbox'
+
+const instance = basicLightbox.create(`
+    <div class="modal">
+        <p>
+            Your first lightbox with just a few lines of code.
+            Yes, it's really that simple.
+        </p>
+    </div>
+`)
+
+instance.show()
