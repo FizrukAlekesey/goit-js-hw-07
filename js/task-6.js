be91d3393c91cf4d361806c4eaabfaf6310a7e62
Fizruk <fizrukprogram@gmail.com>
2024-06-09 16:09:15 +0200

    1

--- js/task-6.js
+++ js/task-6.js
@@ -1,38 +1,44 @@
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
-
-function generateDivSqueres(count) {
-  let createDivSquer = '';
-  for (let i = 0; i < count; i += 1) {
-    createDivSquer += `<div style=' background-color: ${getRandomHexColor()}; width: ${
-      30 + i * 10
-    }px; height: ${30 + i * 10}px;'></div>`;
-  }
-  return createDivSquer;
-}
-
-function destroySqueres() {
-  const squeres = boxes.querySelectorAll('div');
-  squeres.forEach(element => {
-    element.remove();
-  });
-}
-
+let amount = 0;
+const inputNumber = document.querySelector('#controls input');
+inputNumber.addEventListener("input", (event) => {
+  amount = event.currentTarget.value;
+});
 const boxes = document.querySelector('#boxes');
-const create = document.querySelector('button[data-create]');
-const destroy = document.querySelector('button[data-destroy]');
-const input = document.querySelector('input');
-
-create.addEventListener('click', () => {
-  if (input.value < 1 || input.value > 100)
-    return alert('Значення повинно бути від 1 до 100');
-  destroySqueres();
-  boxes.insertAdjacentHTML('beforeend', generateDivSqueres(input.value));
-});
-
-destroy.addEventListener('click', () => {
-  destroySqueres();
-});
+const buttons = document.querySelectorAll('#controls button');
+buttons[0].addEventListener("click", () => createBoxes(amount));
+buttons[1].addEventListener("click", () => destroyBoxes());
+function createBoxes(num) {
+  if (num >= 1 && num <= 100) {
+    boxes.innerHTML = "";
+    let size = 30;
+    // original solution
+    // const elements = [];
+    // for (let i = 1; i <= num; i += 1) {
+    //   const newBox = document.createElement("div");
+    //   newBox.style.width = String(size) + "px";
+    //   newBox.style.height = String(size)+ "px";
+    //   newBox.style.backgroundColor = getRandomHexColor();
+    //   elements.push(newBox);
+    //   size += 10;
+    // }
+    // boxes.append(...elements);
+    // new solution
+    let markup = "";
+    for (let i = 1; i <= num; i += 1) {
+      markup += `<div style="width: ${size}px; height: ${size}px; background-color: ${getRandomHexColor()};"></div>`;
+      size += 10;
+    }
+    boxes.insertAdjacentHTML("afterbegin", markup);
+    inputNumber.value = "";
+  }
+}
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
